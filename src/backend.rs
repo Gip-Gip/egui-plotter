30edfed54b76f626f26252d8fe61c487a62fbb63
tomtuamnuq <76523782+tomtuamnuq@users.noreply.github.com>
2024-01-21 20:47:05 +0100

    update deps egui and eframe to v0.25

--- src/backend.rs
+++ src/backend.rs
@@ -361,7 +361,7 @@ impl<'a> DrawingBackend for EguiBackend<'a> {
         if !galley.is_empty() {
             painter.add(TextShape {
                 angle,
-                ..TextShape::new(rect.min, galley)
+                ..TextShape::new(rect.min, galley, Color32::PLACEHOLDER)
             });
         }
 
